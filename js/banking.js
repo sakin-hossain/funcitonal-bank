1670fcf8dee0397487cc19aea3f7f380aa36c8f6
sakin-hossain <info.sakinh@gmail.com>
2021-08-16 20:41:00 +0600

    finished

--- js/banking.js
+++ js/banking.js
@@ -12,13 +12,19 @@ function updateTotalField(totalFieldId, amount){
     const totalField = document.getElementById(totalFieldId);
     const previousTotal = parseFloat(totalField.innerText);
 
-    totalField.innerText = previousTotal + amount;
+    return totalField.innerText = previousTotal + amount;
+}
+
+function getCurrentBalance(){
+    const balanceTotal =  document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    return previousBalanceTotal;
 }
 
 function updateBalance(amount, isAdd){
     const balanceTotal =  document.getElementById('balance-total');
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const previousBalanceTotal = getCurrentBalance();
     
     if(isAdd == true){
         balanceTotal.innerText = previousBalanceTotal + amount;
@@ -32,19 +38,30 @@ function updateBalance(amount, isAdd){
 // deposit
 document.getElementById('deposit-btn').addEventListener('click', function(){
     const depositAmount = getInputValue('deposit-input');
-    // get and update deposit
+    if(depositAmount > 0){
+        // get and update deposit
     updateTotalField('deposit-total', depositAmount);
     // update balance
     updateBalance(depositAmount, true);
+    }
+    else{
+        alert('please provide positive value');
+    }
 
 })
 
 //withdraw
 
 document.getElementById('withdraw-btn').addEventListener('click', function(){
+    const CurrentBalance = getCurrentBalance();
     const withdrawAmount = getInputValue('withdraw-input')
+    if(withdrawAmount > 0 && withdrawAmount < CurrentBalance){
     // get and update withdraw
     updateTotalField('withdraw-total', withdrawAmount)
     //Update balance
     updateBalance(withdrawAmount, false);
+    }
+    else{
+        alert('please provide positive value');
+    }
 })
---
+++ js/dump.js
@@ -0,0 +1,7 @@
+function add(a,b){
+    const a;
+    const b;
+    const sum = a+b;
+}
+
+sum(5,7);
